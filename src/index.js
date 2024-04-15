af773381ad03c53f3637c27497dcf29720e1a42e
afuap <gpobi2015@gmail.com>
2024-04-15 14:52:17 -0400

    Added basic JavaScript and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "one cup of rice",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
