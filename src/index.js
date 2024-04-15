7e5567cd64e24a087e3004ce7f6057e192206877
afuap <gpobi2015@gmail.com>
2024-04-15 16:57:28 -0400

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "one cup of rice",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let informationInput = document.querySelector("#information");
+  let apiKey = "b0fa3t4066ff24b4o954e79ca4875fa1";
+  let context =
+    "You are a well diverse cook who loves to try out short and simple recipes from African countries. Your mission is to generate a simple recipe in basic HTML. Make sure to follow user instructions below. Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe and NOT at the beginning.";
+  let prompt = `User instructions: Generate a African food recipe about ${informationInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
